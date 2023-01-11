f082b8c47d684fcf8bf4eabe5745163503a80979
yoonisali <yoonismahamoudali@gmail.com>
2023-01-11 13:19:05 -0800

    add HomeController and Index view
    co-authored-by: Brandon Fenk <brandon.fenk99@gmail.com>

--- GameStop/Controllers/HomeController.cs
+++ GameStop/Controllers/HomeController.cs
@@ -1,15 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
+using GameStop.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
-namespace GameStop.Controllers 
+namespace GameStop.Controllers
 {
     public class HomeController : Controller
     {
+      private readonly GameStopContext _db;
+      private readonly UserManager<ApplicationUser> _userManager;
 
-      [HttpGet("/")]
-      public ActionResult Index()
+      public HomeController(UserManager<ApplicationUser> userManager, GameStopContext db)
       {
-        return View();
+        _userManager = userManager;
+        _db = db;
       }
 
+      [HttpGet("/")]
+      public async Task<ActionResult> Index()
+      {
+        Gamer[] gamers = _db.Gamers.ToArray();
+        Dictionary<string,object[]> model = new Dictionary<string, object[]>();
+        model.Add("gamers", gamers);
+        string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        if (currentUser != null)
+        {
+          VideoGame[] videogames = _db.VideoGames
+                      .Where(entry => entry.User.Id == currentUser.Id)
+                      .ToArray();
+          model.Add("videogames", videogames);
+        }
+        return View(model);
+      }
     }
 }
